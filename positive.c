3306fcfba675628699b91afb6968a478042948e0
Gayathri234 <gayathrigaddam942@gmail.com>
2016-08-02 09:41:52 +0530

    Update and rename LICENSE.md to positive.c

---
+++ positive.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter a num:\n");
+scanf("%d"&num);
+if(num>0)
+printf("%d is a positive number"num);
+else if(num<0)
+printf("%d is a negatine number"num);
+return 0;
+}
